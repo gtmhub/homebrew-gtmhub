38fc638cb291829880cf08a3c358a2c3d6983602
petar <petar@gtmhub.com>
2021-01-06 18:24:57 +0200

    updating the formula version

--- Formula/gtmhub.rb
+++ Formula/gtmhub.rb
@@ -1,9 +1,10 @@
 class Gtmhub < Formula
     desc "usefull script that prints Hello World to your Console"
     homepage "https://gtmhub.com"
+    version "0.1.4"
   
   
-    url "https://github.com/gtmhub/homebrew-gtmhub/releases/download/0.1.3/gtmhub_macos.zip", :using => :curl
+    url "https://github.com/gtmhub/homebrew-gtmhub/releases/download/0.1.4/gtmhub_macos.zip", :using => :curl
   
     def install
       bin.install "gtmhub"
