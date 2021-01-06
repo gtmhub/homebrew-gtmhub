cb21395c9ef35de036a99ccc9cff61ffe87b2e55
petar <petar@gtmhub.com>
2021-01-06 19:55:23 +0200

    the download is now pointing to the latest version of gtmhub for macos

--- Formula/gtmhub.rb
+++ Formula/gtmhub.rb
@@ -1,10 +1,8 @@
 class Gtmhub < Formula
     desc "usefull script that prints Hello World to your Console"
     homepage "https://gtmhub.com"
-    version "0.1.5"
   
-  
-    url "https://github.com/gtmhub/homebrew-gtmhub/releases/download/0.1.5/gtmhub_macos.zip", :using => :curl
+    url "https://github.com/gtmhub/gtmhub-cli/releases/latest/download/gtmhub_macos.zip", :using => :curl
   
     def install
       bin.install "gtmhub"
