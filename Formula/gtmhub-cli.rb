5fef10cb916802534a83f2d32f611f0ae62c55cc
petar <petar@gtmhub.com>
2021-01-07 01:55:14 +0200

    updating the cli version and the name

--- Formula/gtmhub-cli.rb
+++ Formula/gtmhub-cli.rb
@@ -1,8 +1,8 @@
 class GtmhubCli < Formula
     desc "The most okr friendly tool in the world"
     homepage "https://gtmhub.com"
-    version "0.1.5"
-    url "https://github.com/gtmhub/homebrew-gtmhub/releases/latest/download/gtmhub_macos.zip", :using => :curl
+    version "0.1.7"
+    url "https://github.com/gtmhub/homebrew-gtmhub/releases/download/v0.1.7/gtmhub_macos.zip", :using => :curl
   
     def install
       bin.install "gtmhub"
