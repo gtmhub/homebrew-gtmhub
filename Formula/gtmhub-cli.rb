1b17e18293659f8d3740e0fb52eea7c56f9e216e
petar <petar@gtmhub.com>
2021-01-07 00:45:54 +0200

    renaming the formula to gtmhub-cli

--- Formula/gtmhub-cli.rb
+++ Formula/gtmhub-cli.rb
@@ -1,7 +1,7 @@
-class Gtmhub < Formula
-    desc "usefull script that prints Hello World to your Console"
+class GtmhubCli < Formula
+    desc "The most okr friendly tool in the world"
     homepage "https://gtmhub.com"
-  
+    version "0.1.5"
     url "https://github.com/gtmhub/homebrew-gtmhub/releases/latest/download/gtmhub_macos.zip", :using => :curl
   
     def install
