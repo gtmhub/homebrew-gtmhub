e084eb85d21fd1cfc6c19810fd1430ecf11c1b26
petar <petar@gtmhub.com>
2021-01-08 20:09:45 +0200

    adding a temp template for generating homebrew formula

---
+++ templates/gtmhub-cli.rb
@@ -0,0 +1,10 @@
+class GtmhubCli < Formula
+    desc "The most okr friendly tool in the world"
+    homepage "https://gtmhub.com"
+    version {{ .Version }}
+    url {{ .DownloadUrl }}, :using => :curl
+  
+    def install
+      bin.install "gtmhub"
+    end
+  end
