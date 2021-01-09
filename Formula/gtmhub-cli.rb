5fb6d7850960be2b7425833f1944164979874e92
petarpetkov <scorpias@gmail.com>
2021-01-09 01:35:43 +0000

    automatic version generator 0.2.1

--- Formula/gtmhub-cli.rb
+++ Formula/gtmhub-cli.rb
@@ -1,7 +1,7 @@
 class GtmhubCli < Formula
     desc "The most okr friendly tool in the world"
     homepage "https://gtmhub.com"
-    version 0.1.1
+    version 0.2.1
     url https://api.github.com/repos/gtmhub/gtmhub-cli/releases/assets/30431842, :using => :curl
   
     def install
