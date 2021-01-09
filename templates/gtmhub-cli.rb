c0eda5a06ee031b392f35107cfbcfe08d5240509
petarpetkov <>
2021-01-09 16:32:21 +0200

    adding a template file to generate future formulas

---
+++ templates/gtmhub-cli.rb
@@ -0,0 +1,11 @@
+class GtmhubCli < Formula
+  desc "The most okr friendly tool in the world"
+  homepage "https://gtmhub.com"
+  version {{ .Version }}
+  sha256 {{ .Sha256 }}
+  url {{ .DownloadUrl }}, :using => :curl
+
+  def install
+    bin.install "gtmhub"
+  end
+end
