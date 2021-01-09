7acdd2ba92c6b9686166b84353b1313f93617546
petarpetkov <>
2021-01-09 16:37:37 +0200

    updating the template

--- templates/gtmhub-cli.rb
+++ templates/gtmhub-cli.rb
@@ -1,11 +1,11 @@
 class GtmhubCli < Formula
-  desc "The most okr friendly tool in the world"
-  homepage "https://gtmhub.com"
-  version {{ .Version }}
-  sha256 {{ .Sha256 }}
-  url {{ .DownloadUrl }}, :using => :curl
+    desc "The most okr friendly tool in the world"
+    homepage "https://gtmhub.com"
+    version "{{ .Version }}"
+    sha256 "{{ .Sha256 }}"
+    url "{{ .DownloadUrl }}", :using => :curl
 
-  def install
-    bin.install "gtmhub"
+    def install
+      bin.install "gtmhub"
+    end
   end
-end
