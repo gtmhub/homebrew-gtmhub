6dc16a8c0aa16fae3c675fd11ff111b5dcd634d9
petarpetkov <>
2021-01-22 17:48:08 +0000

    automatic version generator 0.1.6

--- Formula/gtmhub-cli.rb
+++ Formula/gtmhub-cli.rb
@@ -1,9 +1,9 @@
 class GtmhubCli < Formula
     desc "The most okr friendly tool in the world"
     homepage "https://gtmhub.com"
-    version "0.1.5"
-    sha256 "485817173fefd355b1c97717530f87984a9854e908c8431d0f0b14421ba18ee9"
-    url "https://github.com/gtmhub/gtmhub-cli/releases/download/0.1.5/gtmhub_macos.zip", :using => :curl
+    version "0.1.6"
+    sha256 "25bcfb0e532c4c0c45d95742396a53dc85cab631786163d38f40241d98d5a13c"
+    url "https://github.com/gtmhub/gtmhub-cli/releases/download/0.1.6/gtmhub_macos.zip", :using => :curl
 
     def install
       bin.install "gtmhub"
