3df3c35f28404bfff99051a6968047989feb56c8
petarpetkov <petar@gtmhub.com>
2021-11-25 14:50:37 +0000

    automatic version generator 0.1.0

--- Formula/gtmhub-codegen.rb
+++ Formula/gtmhub-codegen.rb
@@ -1,9 +1,9 @@
 class GtmhubCli < Formula
     desc "Gtmhub extensibility code generation tool"
     homepage "https://gtmhub.com"
-    version "0.1.7"
-    sha256 "347ca5f8609950d03dc8e4e02ef387a538922b4fe33d290f5ec7c1fe43e31357"
-    url "https://github.com/gtmhub/gtmhub-cli/releases/download/0.1.7/gtmhub_macos.zip", :using => :curl
+    version "0.1.0"
+    sha256 "111c78d091375e70d6871ebf37ff7090ac3b8e2dfa88aedf2ec3bbf3bbbbe2dd"
+    url "https://github.com/gtmhub/gtmhub-codegen/releases/download/0.1.0/gtmhub_codegen_macos.zip", :using => :curl
 
     def install
       bin.install "gtmhub-codegen"
