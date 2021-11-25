d1a44c0180e813817bd8253a8892e70285bc3ee5
petarpetkov <petar@gtmhub.com>
2021-11-25 16:07:18 +0200

    adding a code gen template

---
+++ templates/gtmhub-codegen.rb
@@ -0,0 +1,11 @@
+class GtmhubCli < Formula
+    desc "Gtmhub extensibility code generation tool"
+    homepage "https://gtmhub.com"
+    version "{{ .Version }}"
+    sha256 "{{ .Sha256 }}"
+    url "{{ .DownloadUrl }}", :using => :curl
+
+    def install
+      bin.install "gtmhub-codegen"
+    end
+  end
